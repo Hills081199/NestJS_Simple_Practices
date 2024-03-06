a33ee2a7a99b49f4a15d21bb3b606cbcde38751d
nguyen ngoc hieu <hieubait99@gmail.com>
2024-03-06 10:40:00 +0700

    create schemas for category and post

---
+++ nestjs_crud_manytomany_relation_mongodb/src/schemas/category.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document, ObjectId } from "mongoose";
+import { Post } from "./post.schema";
+import { Transform } from "class-transformer";
+
+export type CategoryDocument = Category & Document
+
+@Schema()
+export class Category
+{
+    @Transform(({ value }) => value.toString())
+    _id: ObjectId;
+
+    @Prop({ required: true })
+    title: string;
+
+    @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: Post.name}])
+    posts: Post;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
---
+++ nestjs_crud_manytomany_relation_mongodb/src/schemas/post.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document, ObjectId } from "mongoose";
+import { Category } from "./category.schema";
+import { Transform } from "class-transformer";
+
+export type PostDocument = Post & Document
+
+@Schema()
+export class Post
+{
+    @Transform(({value}) => value.toString())
+    _id: ObjectId;
+
+    @Prop({ required: true })
+    title: string;
+
+    @Prop()
+    description: string;
+
+    @Prop([{ type: mongoose.Schema.Types.ObjectId, ref: Category.name }])
+    categories: Category;
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
